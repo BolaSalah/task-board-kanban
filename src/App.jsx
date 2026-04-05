eb18dddc42f75f8d8db2d8e00742da8ccac2bd4b
Bola Salah <103209637+BolaSalah@users.noreply.github.com>
2026-04-05 21:13:33 +0200

    feat: complete header UI and make tasks count dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import Header from './components/Header'
+import { Box } from '@mui/material'
 
 const App = () => {
+
   return (
-    <div>App</div>
+    <>
+
+      {/* Main Container */}
+      <Box
+        sx={{ bgcolor: "#f4f6f8", minHeight: "100vh" }}
+      >
+
+        {/* Header component */}
+        <Box
+          sx={{ padding: "20px" }}
+        >
+          <Header />
+        </Box>
+
+        {/* Sepration Line below the Header */}
+        <Box sx={{ height: "1px", bgcolor: "#d9d9d9", width: "100%" }}>
+
+        </Box>
+
+      </Box>
+
+    </>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,127 @@
+import React from 'react'
+import { Box, CircularProgress, InputAdornment, TextField, Typography } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search';
+import { useQuery } from '@tanstack/react-query'
+import { getTasks } from '../services/taskService';
+
+const Header = () => {
+
+    const { data: tasks = [], isLoading, isError } = useQuery({
+        queryKey: ['tasks'],
+        queryFn: getTasks,
+    })
+
+    if (isLoading) {
+        return (
+            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh', bgcolor: "#f4f6f8" }}>
+                <CircularProgress />
+            </Box>
+        )
+    }
+
+    if (isError) {
+        return (
+            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh', bgcolor: "#f4f6f8" }}>
+                <Typography color="error">
+                    Error loading tasks. Make sure json-server is running on port 4000!
+                </Typography>
+            </Box>
+        )
+    }
+
+
+    return (
+        <Box sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: "100%",
+        }}
+        >
+            {/* Left side: Logo, title and Length of tasks */}
+            <Box sx={{ display: "flex", alignItems: "center", gap: "6px" }}>
+                {/* Logo */}
+                {/* <Box
+                    sx={{
+                        width: 35,
+                        height: 35,
+                        bgcolor: '#2f54eb',
+                        borderRadius: '8px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        color: 'white',
+                        fontWeight: 'bold'
+                    }}
+                >
+                    KB
+                </Box> */}
+                <Box
+                    sx={{
+                        width: 20,
+                        height: 20,
+                        bgcolor: '#2f54eb',
+                        borderRadius: '12px',
+                        display: 'grid',
+                        gridTemplateColumns: 'repeat(2, 1fr)',
+                        gap: '2px',
+                        p: '10px'
+                    }}
+                >
+                    <Box sx={{ border: "2px solid white", borderRadius: '2px' }}></Box>
+                    <Box sx={{ border: "2px solid white", borderRadius: '2px' }}></Box>
+                    <Box sx={{ border: "2px solid white", borderRadius: '2px' }}></Box>
+                    <Box sx={{ border: "2px solid white", borderRadius: '2px' }}></Box>
+                </Box>
+
+                {/* title and Length of tasks */}
+                <Box>
+                    {/* title  */}
+                    <Typography variant="subtitle1" sx={{ fontWeight: '700', lineHeight: 1.2, color: '#1f1f1f' }}>
+                        KANBAN BOARD
+                    </Typography>
+                    {/* Length of tasks */}
+                    <Typography variant="caption" sx={{ color: '#8c8c8c', fontWeight: '700' }}>
+                        {tasks.length} {tasks.length === 1 ? "task" : "tasks"}
+                    </Typography>
+                </Box>
+            </Box>
+
+            {/* Right side:Search Bar */}
+            <TextField
+                variant="outlined"
+                size="small"
+                placeholder="Search tasks..."
+                sx={{
+                    width: 260,
+                    bgcolor: '#f0f2f5',
+                    borderRadius: '8px',
+                    '& .MuiOutlinedInput-root': {
+                        borderRadius: '8px',
+                        '& fieldset': {
+                            borderColor: 'transparent',
+                        },
+                        '&:hover fieldset': {
+                            borderColor: 'transparent',
+                        },
+                        '&.Mui-focused fieldset': {
+                            borderColor: '#2f54eb',
+                        },
+                    },
+                    '& .MuiInputBase-input': {
+                        padding: '8px 12px',
+                    }
+                }}
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <SearchIcon sx={{ color: '#bfbfbf', fontSize: 20 }} />
+                        </InputAdornment>
+                    ),
+                }}
+            />
+        </Box>
+    )
+}
+
+export default Header
